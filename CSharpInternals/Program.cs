640916f77089e1c770a8f799cd544d78bd59627f
sruthi45 <sirekollam.sruthi18@gmail.com>
2022-01-05 00:38:06 +0530

    Add project files.

---
+++ CSharpInternals/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpInternals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Enumerable.Range(1, 100);
+
+            var result=list.Where(x=>x == 1);
+            var list1 = new BlockingCollection<int>();
+            Parallel.ForEach(list, x =>
+            {
+                list1.Add(x);
+            });
+
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+
+            //try
+            //{
+            //    var task1 = Task.Run(() => { throw new CustomException("This exception is expected!"); });
+            //    var task2 = Task.Run(() => { throw new CustomException("This exception is expected2!"); });
+
+            //    try
+            //    {
+            //        task1.Wait();
+            //        task2.Wait();
+            //    }
+            //    catch (Exception ae)
+            //    {
+            //        // Call the Handle method to handle the custom exception,
+            //        // otherwise rethrow the exception.
+            //        //ae.Handle(ex =>
+            //        //{
+            //        //    if (ex is CustomException)
+            //        //        Console.WriteLine(ex.Message);
+            //        //    return ex is CustomException;
+            //        //});
+            //        Console.WriteLine(ae.Message);
+            //    }
+            //}
+            //catch (AggregateException ex)
+            //{
+            //    ex.Handle((x) =>
+            //    {
+            //        if (x is Exception)
+            //        {
+            //            Console.WriteLine(x.Message);
+            //            return true;
+            //        }
+            //        return false;
+            //    });
+            //}
+
+            Console.ReadLine();
+        }
+    }
+
+    public class CustomException : Exception
+    {
+        public CustomException(String message) : base(message)
+        { }
+    }
+
+    public partial class SomethingPartial
+    {
+        partial void GetData(string value);
+    }
+
+    public partial class SomethingPartial
+    {
+        partial void GetData(string value)
+        {
+             //do something
+        }
+    }
+
+    public class Sample : ISample<Service>
+    {
+        public Service GetT(int value)
+        {
+            return (Service)Activator.CreateInstance(typeof(Service));
+        }
+    }
+
+    public interface ISample<out T> where T: class,new()
+    {
+        T GetT(int value);
+
+        //void SetT(T value);
+    }
+
+    public interface ISample1<in T>
+    {
+        string GetT(T value);
+
+        //T GetT1(int value);
+    }
+
+    public static class SomeExtension
+    {
+        public static string ToInt(this int value)
+        {
+            return value.ToString();
+        }
+    }
+
+    public class Service : IServiceA,IServiceB
+    {
+        public string GetData(int value)
+        {
+            return $"Value is {value}";
+        }
+
+        string IServiceA.GetData(int value)
+        {
+            return $"Value is {value}";
+        }
+    }
+
+    public interface IServiceA
+    {
+        string GetData(int value);
+    }
+
+    public interface IServiceB
+    {
+        string GetData(int value);
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Bench>();
+            Console.ReadLine();
+
+        }
+    }
+
+    [MemoryDiagnoser]
+    [RankColumn]
+    public class Bench
+    {
+        [Params(100, 200)]
+        public int A { get; set; }
+
+        [Params(100, 200)]
+        public int B { get; set; }
+
+
+        [Benchmark(Baseline =true)]
+        public void Sum()
+        {
+            var SomeClass = new SomeClass();
+            SomeClass.Sum(A, B);
+        }
+
+        [Benchmark]
+        public void Sum1()
+        {
+            var SomeClass = new SomeClass();
+            SomeClass.Sum1(A, B);
+        }
+    }
+
+
+    public class SomeClass
+    {
+
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public virtual int Sum1(int a, int b) => a + b;
+    }
+}
